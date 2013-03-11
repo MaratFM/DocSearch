468be9b21d4d2688b0b72d96bc9aef7f4fbacdfc
Marat Khabibullin <maratfm@gmail.com>
2013-03-11 22:19:33 +0400

    Initial Commit

---
+++ DocSearch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DocSearch
+//
+//  Created by Marat Khabibullin on 11.03.13.
+//  Copyright (c) 2013 Marat Khabibullin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
